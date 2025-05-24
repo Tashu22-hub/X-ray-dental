8bca9efb9d82144ed33596aa7387bfa21a7badb9
Tashu prajapati <tashu.ug22@nsut.ac.in>
2025-05-24 13:26:42 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://x-ray-dental-frontend.vercel.app" , "http://localhost:5173"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
